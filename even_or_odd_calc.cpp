643a72b7b8f5854db9ac03ccae6a3ab7e569a4fa
MD RASHID ALI HAIDER <rashidnrlbhilai@gmail.com>
2025-02-26 15:46:39 +0530

    Add files via upload

---
+++ even_or_odd_calc.cpp
@@ -0,0 +1,14 @@
+/*created by @mdrashidah*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number"<<endl;
+    cin>>n;
+    if(n%2==0){
+        cout<<"The number is Even"<<endl;
+    }
+    else{
+        cout<<"The number is Odd"<<endl;
+    }
+}
